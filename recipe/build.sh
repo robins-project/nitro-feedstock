da4af23721c5c70ded1adbd8c5c8b62270552b12
v-turin <vladimir.turin@opencascade.com>
2018-10-12 16:35:20 +0300

    Gcc 5.4.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,14 @@
 mkdir build
 cd build
 
-# build with c++11
-export CXXFLAGS=$(echo $CXXFLAGS | sed "s/-std=c++17//")
-export CXXFLAGS="$CXXFLAGS -std=c++11"
+# # build with c++11
+# export CXXFLAGS=$(echo $CXXFLAGS | sed "s/-std=c++17//")
+# export CXXFLAGS="$CXXFLAGS -std=c++11"
 
 cmake -G "Ninja" \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE:STRING=Release \
+      -DCMAKE_CXX_FLAGS:STRING=-std=c++11 \
       ..
 
 ninja install
