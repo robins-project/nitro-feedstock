813fcc2e6e1578fd5040a70598a7ac478b02fe6f
v-turin <vladimir.turin@opencascade.com>
2018-10-15 18:53:35 +0300

    Use native compiler

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,13 +4,12 @@ mkdir build
 cd build
 
 # # build with c++11
-# export CXXFLAGS=$(echo $CXXFLAGS | sed "s/-std=c++17//")
-# export CXXFLAGS="$CXXFLAGS -std=c++11"
+export CXXFLAGS=$(echo $CXXFLAGS | sed "s/-std=c++[0-9][0-9]//")
+export CXXFLAGS="$CXXFLAGS -std=c++11"
 
 cmake -G "Ninja" \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE:STRING=Release \
-      -DCMAKE_CXX_FLAGS:STRING=-std=c++11 \
       ..
 
 ninja install
